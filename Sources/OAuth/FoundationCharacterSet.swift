6cbef532d64b052458930f8f8c4063466372df29
Jaehong Kang <sinoru@me.com>
2016-12-11 16:10:55 +0900

    Workaround for SR-3282

--- Sources/OAuth/FoundationCharacterSet.swift
+++ Sources/OAuth/FoundationCharacterSet.swift
@@ -3,7 +3,16 @@
 //  OAuth
 //
 //  Created by Sinoru on 2016. 12. 11..
-//
+//  Copyright © 2016 Sinoru. All rights reserved.
 //
 
 import Foundation
+
+extension CharacterSet {
+    
+    static var urlUnreservedCharacters: CharacterSet {
+        var urlUnreservedCharacters = CharacterSet.alphanumerics
+        urlUnreservedCharacters.insert(charactersIn: "-._~")
+        return urlUnreservedCharacters
+    }
+}
